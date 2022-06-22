c103d28f5f2f66d25277b455c5388129f8708eab
tunaytoksoz <tunaytoksoz@hotmail.com>
2022-06-22 11:33:45 +0300

    Initial Commit

---
+++ usdzInteractive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  usdzInteractive
+//
+//  Created by Tunay Toksöz on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
